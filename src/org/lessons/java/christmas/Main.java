18b28892d87caafa9639c2f6304fd46193272eec
mattialicausi <licausivitomattia@gmail.com>
2023-03-16 14:06:13 +0100

    implementato metodo main in cui chiedo all'utente di inserire i desideri all'interno della lista fin quando decide di uscire

--- src/org/lessons/java/christmas/Main.java
+++ src/org/lessons/java/christmas/Main.java
@@ -1,7 +1,31 @@
 package org.lessons.java.christmas;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        // Collection list per inserire la lista dei desideri
+        List<String> wishList = new ArrayList<>();
+
+        boolean stop = false;
+
+        //chiedo all'utente di inserire un desiderio fin quando decide di uscire
+        while (!stop) {
+            System.out.print("Inserisci un desiderio: ");
+            String newWish = scan.nextLine();
+            wishList.add(newWish);
+
+            System.out.println(wishList);
+            System.out.print("Vuoi uscire? (y/n): ");
+            stop = scan.nextLine().equalsIgnoreCase("y");
+        }
+
+        System.out.println("Arrivederci! ");
 
     }
 }
