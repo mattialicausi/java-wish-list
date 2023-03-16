ab8dfb099adc57a772a0dcc2f4f5740c41582d4e
mattialicausi <licausivitomattia@gmail.com>
2023-03-16 15:18:57 +0100

    implementato metodo send che verifica se inviare o meno la lista con le relative eccezioni

---
+++ src/org/lessons/java/christmas/ChristmasLetter.java
@@ -0,0 +1,71 @@
+package org.lessons.java.christmas;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class ChristmasLetter {
+    private String name;
+    private String address;
+
+    List <String> wishList = new ArrayList<>();
+
+    public ChristmasLetter(String name, String address, List<String> wishList) {
+        this.name = name;
+        this.address = address;
+        this.wishList = wishList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public List<String> getWishList() {
+        return wishList;
+    }
+
+    public void setWishList(List<String> wishList) {
+        this.wishList = wishList;
+    }
+
+    //metodi
+    public List<String> send() throws Exception {
+        if (wishList.size() > 5) {
+            throw new Exception("Spiacente, puoi mandare massimo 5 desideri");
+        }
+
+        Random rand = new Random();
+        double num = rand.nextInt(2);
+
+        if (num == 0) {
+            throw new Exception("Spiacente sei stato cattivo, non puoi mandare la lista");
+        } else {
+            System.out.println("Ciao: " + getName() + " la tua lista dei desideri: " + wishList  + " è stata spedita qui " + getAddress());
+        }
+
+        return wishList;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "ChristmasLetter{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", wishList=" + wishList +
+                '}';
+    }
+}
--- src/org/lessons/java/christmas/Main.java
+++ src/org/lessons/java/christmas/Main.java
@@ -1,31 +1,54 @@
 package org.lessons.java.christmas;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner scan = new Scanner(System.in);
 
-        // Collection list per inserire la lista dei desideri
         List<String> wishList = new ArrayList<>();
 
         boolean stop = false;
 
-        //chiedo all'utente di inserire un desiderio fin quando decide di uscire
-        while (!stop) {
-            System.out.print("Inserisci un desiderio: ");
-            String newWish = scan.nextLine();
-            wishList.add(newWish);
+        System.out.print("Nome? ");
+        String name = scan.nextLine();
 
-            System.out.println(wishList);
+        System.out.print("Indirizzo? ");
+        String address = scan.nextLine();
+
+        while (!stop) {
+            try {
+                System.out.print("Inserisci un desiderio: ");
+                String newWish = scan.nextLine();
+                wishList.add(newWish);
+                Collections.sort(wishList);
+
+                System.out.println(wishList);
+
+                if (wishList.size() >= 5) {
+                    throw new Exception("Spiacente, puoi mandare massimo 5 desideri");
+                }
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+            }
+
             System.out.print("Vuoi uscire? (y/n): ");
             stop = scan.nextLine().equalsIgnoreCase("y");
         }
 
         System.out.println("Arrivederci! ");
 
+        ChristmasLetter newWishList = new ChristmasLetter(name, address, wishList);
+
+        try {
+            newWishList.send();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+
     }
+
 }
